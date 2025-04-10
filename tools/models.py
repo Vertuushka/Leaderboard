7ed4b12af83a497de2aea47aa4d50cc98d617e11
Kiryl <kirillyasniy@gmail.com>
2025-04-10 22:14:23 +0200

    TOOLS: app initialization, participants, shows and performances models

---
+++ tools/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+class Participant(models.Model):
+    name = models.CharField(max_length=100)
+    song = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    img = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.country} - {self.name}'
+
+class Show(models.Model):
+    SHOW_TYPES = [
+        ('SF1', 'Semi-Final 1'),
+        ('SF2', 'Semi-Final 2'),
+        ('GF', 'Grand Final'),
+    ]
+    name = models.CharField(max_length=3, choices=SHOW_TYPES, unique=True)
+
+    def __str__(self):
+        return dict(self.SHOW_TYPES)[self.name]
+
+class Performance(models.Model):
+    participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
+    show = models.ForeignKey(Show, on_delete=models.CASCADE)
+    order = models.IntegerField()
+    # semi-final only
+    points = models.IntegerField(null=True, blank=True)
+    passed = models.BooleanField(null=True, blank=True) 
+    # grand final only
+    jury = models.IntegerField(null=True, blank=True)
+    votes = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        unique_together = ('participant', 'show')
+
+    def __str__(self):
+        return f'{self.participant} - {self.show}'
