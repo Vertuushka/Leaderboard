eeb9ca2bb38c27ae47dbb1430148afcc0e236361
Kiryl <kirillyasniy@gmail.com>
2025-04-10 23:55:55 +0200

    TOOLS: adding JSON file containing premade DB data

--- tools/models.py
+++ tools/models.py
@@ -4,7 +4,7 @@ class Participant(models.Model):
     name = models.CharField(max_length=100)
     song = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
-    img = models.CharField(max_length=100)
+    img = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return f'{self.country} - {self.name}'
