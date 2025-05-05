e2768f83e246487ba7020ca38da7576d517b579b
Kiryl <kirillyasniy@gmail.com>
2025-05-05 20:13:31 +0200

    img fixes + connecting some JS with index

--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 from .utils import *
 from tools.models import *
 from . models import GlobalSettings
+import json
 
 # Create your views here.
 def index(request):
@@ -11,7 +12,19 @@ def index(request):
         show = GlobalSettings.objects.get(id=1).state
         context['show'] = show
         context['performances'] = Performance.objects.filter(show=show)
-        context['participants'] = Participant.objects.all()
+        context['participants'] = Participant.objects.filter(performance__show=show).distinct()
+        performances = Performance.objects.filter(show=show).order_by('id')
+        performances_data = []
+        for performance in performances:
+            performances_data.append({
+                "id": performance.id,
+                "passed": performance.passed,
+                "name": performance.participant.name,
+                "song": performance.participant.song,
+                "country": performance.participant.country,
+                "image": performance.participant.img,
+            })
+        context['performances_data'] = json.dumps(performances_data)
     except Exception as e:
         error = build_error_msg(e)
         context['error'] = error    
