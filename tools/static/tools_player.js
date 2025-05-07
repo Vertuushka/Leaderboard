bd0592d15aa19b8c9f4b09bddf185cd02b4deb04
Kiryl <kirillyasniy@gmail.com>
2025-05-07 20:55:49 +0200

    applying color to selected grade button

--- tools/static/tools_player.js
+++ tools/static/tools_player.js
@@ -3,8 +3,19 @@ import { ELEMENTS } from "./live.js";
 import { VoteHandler } from "./send_handlers.js";
 
 function RegisterElements() {
-    const voteBtns = document.querySelectorAll(".voteBtn");
+    const voteBtns = [...document.querySelectorAll(".voteBtn")];
+    let groups = [];
+    for (let i = 0; i < voteBtns.length; i += 10) {
+        groups.push(voteBtns.slice(i, i + 10));
+    }
     voteBtns.forEach((btn, i) => {
+        btn.addEventListener("click", () => {
+            const block = groups.find(groups => groups.includes(btn))
+            block.forEach(btn => {
+                btn.classList.remove("active");
+            })
+            btn.classList.add("active");
+        })
         tools.RegisterElement(btn, tools.ButtonElement, new VoteHandler(), `voteBtn${i}`);
     })
 }
