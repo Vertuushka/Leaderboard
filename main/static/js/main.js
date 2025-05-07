00a7a039bf1f68d7170a626d4a18cc20be4ce88c
Erikas J <erikas.privat3043@gmail.com>
2025-05-07 04:16:09 +0200

    Mobile styling

--- main/static/js/main.js
+++ main/static/js/main.js
@@ -1,9 +1,11 @@
-function setBodyHeight() {
-    document.body.style.height = window.innerHeight + 'px';
-}
-
-// Set body height initially
-setBodyHeight();
-
-// Update on window resize
-window.addEventListener('resize', setBodyHeight);
+document.addEventListener("DOMContentLoaded", function() {
+    function setBodyHeight() {
+        document.body.style.height = window.innerHeight + 'px';
+    }
+    
+    // Set body height initially
+    setBodyHeight();
+    
+    // Update on window resize
+    window.addEventListener('resize', setBodyHeight);
+});
