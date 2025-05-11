5da73bf83b71bc96a09e950cb77036cc7fc715ec
Erikas J <102188175+eerikazz@users.noreply.github.com>
2025-05-12 01:07:08 +0200

    Update asgi.py

--- leaderboard/asgi.py
+++ leaderboard/asgi.py
@@ -8,18 +8,21 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
+import django
+from django.core.asgi import get_asgi_application
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'leaderboard.settings')
 
-import django
 django.setup()
 
-from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from tools import routing
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket":AuthMiddlewareStack(
         URLRouter (
             routing.websocket_urlpatterns
