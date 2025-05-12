e04792956cf9417dba85d6c3910b018c4d6b76fb
Erikas J <102188175+eerikazz@users.noreply.github.com>
2025-05-12 02:57:51 +0200

    Update settings.py

--- leaderboard/settings.py
+++ leaderboard/settings.py
@@ -138,3 +138,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "127.0.0.1,localhost").split(",")
